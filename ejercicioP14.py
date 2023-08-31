1de84e1be99ea55244e662aeb6e3072938044b63
Durango1405 <143657315+Durango1405@users.noreply.github.com>
2023-08-30 23:31:37 -0500

    Add files via upload

---
+++ ejercicioP14.py
@@ -0,0 +1,8 @@
+# leer número
+numero = float(input("ingrese un numero entero o decimal: ")) #float para que se puedan ingresar números decimales
+# calcular cuadrado y cubo de dicho número
+cuadrado = numero ** 2
+cubo = numero ** 3
+# resultados
+print(f"El cuadrado de {numero} es: {cuadrado}")
+print(f"El cubo de {numero} es: {cubo}")
